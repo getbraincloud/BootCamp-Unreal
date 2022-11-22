d09cf21bdd44e8286afd04fd0291741c7cb241e3
Brad-F <bradleyf@bitheads.com>
2022-11-22 13:21:56 -0500

    Initial commit for Lesson-31

---
+++ Source/SpaceShooter/Achievement.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Achievement.h"
+#include "NetworkManager.h"
+#include "SpaceShooterGameModeBase.h"
+
+
+Achievement::Achievement(const FString& achievementID, const FString& title, const FString& description, const FString& status, int sortOrder) :
+    m_AchievementID(achievementID),
+    m_Title(title),
+    m_Description(description),
+    m_Status(status == "AWARDED" ? Status::Awarded : Status::NotAwarded),
+    m_SortOrder(sortOrder)
+{
+}
+
+void Achievement::Award(ASpaceShooterGameModeBase* gameMode)
+{
+    if(m_Status == Status::NotAwarded)
+    {
+        m_Status = Status::Awarded;
+        gameMode->GetNetworkManager()->AwardAchievement(this);
+    }
+}
+
+bool Achievement::IsAwarded() const
+{
+    return m_Status == Status::Awarded;
+}
+
+FString Achievement::GetStatusString() const
+{
+    if (m_Status == Status::Awarded)
+        return TEXT("Earned");
+    return TEXT("");
+}
+
+FString Achievement::GetAchievementID() const
+{
+    return m_AchievementID;
+}
+
+FString Achievement::GetTitle() const
+{
+    return m_Title;
+}
+
+FString Achievement::GetDescription() const
+{
+    return m_Description;
+}
+
+int Achievement::GetSortOrder() const
+{
+    return m_SortOrder;
+}
+
